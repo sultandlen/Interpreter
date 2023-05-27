35519eb1e2ad9db4b6d979fe9a4dfa80ae1fb937
Sultan Delen <99362897+sultandlen@users.noreply.github.com>
2023-05-27 13:42:04 +0300

    Create isKeyword method

--- interpreter.c
+++ interpreter.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdbool.h>
 typedef enum {
    IDENTIFIER,
    INT_CONST,
@@ -12,3 +14,14 @@ typedef struct {
     TokenType type;
     char lexeme[];
 } Token;
+bool isKeyword (char str[]) {
+  const char* KEYWORDS[] = {"new", "int", "text", "size", "subs", "locate", "insert", "override", "read", "write",
+                           "from", "to", "input", "output", "asText", "asString"};
+  for (int i = 0; i < sizeof(KEYWORDS) /sizeof(KEYWORDS[0]); i++) {
+    if (strcmp(KEYWORDS[i], str) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
